5e22e63b106b59396c5dd41990e8a695d45598ed
Vitor <vitornuneschagas2016@gmail.com>
2024-03-13 22:54:33 -0300

    custom response from bot, organizate classes, validate

--- src/main/java/com/github/chatbot/WebhookController.java
+++ src/main/java/com/github/chatbot/WebhookController.java
@@ -18,8 +18,7 @@ import java.util.Objects;
 @RestController
 public class WebhookController {
 
-    private static final String PAGE_ACCESS_TOKEN = "EAANItlDtKeEBOZCIKQDqRDXpZBPCQ5QDlZCYfZAQ0s2De5DHSCO9192tCUhVvfOPGGsp6ahlJYJz8ZBwQgkAlOOPbVbFVIEAqUZCGI5tlWzyhzbjUif7fMRZBAvRZCj9ifBwBI7qR146hnpUCFqpikooDM3DydhZAjxZAaDG1G4dczqmvZAk4tjn2W4RehBZCoZAoZBWc960GBMefenoRy8ZBMxPbUqCPAZD";
-
+    public WebhookService webhookService = new WebhookService();
     @GetMapping("/webhook")
     public ResponseEntity<String> teste(@RequestParam("hub.mode") String hub, @RequestParam("hub.challenge") String challange, @RequestParam("hub.verify_token") String token ){
         if(Objects.equals(token, "banana")){
@@ -32,60 +31,11 @@ public class WebhookController {
     public ResponseEntity<String> teste2(HttpEntity<String> request) throws IOException {
         String json = request.getBody();
         JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
-
-
-            String psid = jsonObject.get("entry")
-                    .getAsJsonArray()
-                    .get(0)
-                    .getAsJsonObject()
-                    .get("messaging")
-                    .getAsJsonArray()
-                    .get(0)
-                    .getAsJsonObject()
-                    .get("sender")
-                    .getAsJsonObject()
-                    .get("id")
-                    .getAsString();
-
-            String message = jsonObject.get("entry")
-                    .getAsJsonArray()
-                    .get(0)
-                    .getAsJsonObject()
-                    .get("messaging")
-                    .getAsJsonArray()
-                    .get(0)
-                    .getAsJsonObject()
-                    .get("message")
-                    .getAsJsonObject()
-                    .get("text")
-                    .getAsString();
-
-                System.out.println(psid + " " + message);
-            String body = "{\n" +
-                    "  \"recipient\":{\n" +
-                    "    \"id\":\"7397663706980330\"\n" +
-                    "  },\n" +
-                    "  \"messaging_type\": \"RESPONSE\",\n" +
-                    "  \"message\":{\n" +
-                    "    \"text\":\"AUTOMATICO\"\n" +
-                    "  }\n" +
-                    "}\n";
-            URL url = new URL("https://graph.facebook.com/v19.0/206424369230843/messages?access_token=" + PAGE_ACCESS_TOKEN);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-            conn.setRequestProperty("Content-type", "application/json");
-            try(DataOutputStream dos = new DataOutputStream(conn.getOutputStream())){
-                dos.writeBytes(body);
-            }
-            try(BufferedReader bf = new BufferedReader(new InputStreamReader(conn.getInputStream()))){
-                String line;
-                while((line = bf.readLine()) != null){
-                    System.out.println(line);
-                }
-            }
-        return new ResponseEntity<>(HttpStatus.OK);
-
+        if(Objects.equals(jsonObject.get("object").getAsString(), "page" )){
+            webhookService.setStrings(jsonObject);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
 }
--- src/main/java/com/github/chatbot/WebhookService.java
+++ src/main/java/com/github/chatbot/WebhookService.java
@@ -1,6 +1,89 @@
 package com.github.chatbot;
 
+import com.google.gson.JsonObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 public class WebhookService {
-    public void checkMessage(String psid, String message){}
-    public void sendMessage(String psid, String message){}
+    private static final String PAGE_ACCESS_TOKEN = "EAANItlDtKeEBO2BbvIFGJUJNU22580QPLAKG8CauZBDLcnZAZAqSFEy4MxBaAZBUYWKfsNHX7WpALa8YLG1LQPWulwQGXnXm3zYgdP00yc1JqToCs5AFKOu9OvPPS8M65U2MiQG4XjZC3qqwMgkmfbKxH0ozOq0aBfPW4ZBvuvMo2qaw3a2SttfG5UMhB6jFEsP0uVa76l2jJ5osRpZBXELbV0ZD";
+    private  String psid;
+    private String msg = "";
+    private String message;
+    private String body = "Desculpe, nao entendi";
+    public void setStrings(JsonObject jsonObject) throws IOException {
+        this.psid = jsonObject.get("entry")
+                .getAsJsonArray()
+                .get(0)
+                .getAsJsonObject()
+                .get("messaging")
+                .getAsJsonArray()
+                .get(0)
+                .getAsJsonObject()
+                .get("sender")
+                .getAsJsonObject()
+                .get("id")
+                .getAsString();
+        this.message = jsonObject.get("entry")
+                .getAsJsonArray()
+                .get(0)
+                .getAsJsonObject()
+                .get("messaging")
+                .getAsJsonArray()
+                .get(0)
+                .getAsJsonObject()
+                .get("message")
+                .getAsJsonObject()
+                .get("text")
+                .getAsString();
+        checkMessage();
+    };
+    public void checkMessage() throws IOException {
+        if(message.contains("nome") || message.contains("chama")){
+            msg = "Me chamo Vitor";
+        }else if(message.contains("idade") || message.contains("anos")){
+            msg = "Tenho 18 anos";
+        }else{
+            msg = "Nao entendi";
+        }
+        sendMessage();
+    }
+    public void sendMessage() throws IOException {
+        URL url = new URL("https://graph.facebook.com/v19.0/206424369230843/messages?access_token=" + PAGE_ACCESS_TOKEN);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+        conn.setRequestProperty("Content-type", "application/json");
+        try(DataOutputStream dos = new DataOutputStream(conn.getOutputStream())){
+            dos.writeBytes(body(msg));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try(BufferedReader bf = new BufferedReader(new InputStreamReader(conn.getInputStream()))){
+            String line;
+            while((line = bf.readLine()) != null){
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String body(String mensagem){
+       return "{\n" +
+                "  \"recipient\":{\n" +
+                "    \"id\":\"" + psid + "\"\n" +
+                "  },\n" +
+                "  \"messaging_type\": \"RESPONSE\",\n" +
+                "  \"message\":{\n" +
+                "    \"text\":\"" + mensagem + "\"\n" +
+                "  }\n" +
+                "}\n";
+    }
 }
