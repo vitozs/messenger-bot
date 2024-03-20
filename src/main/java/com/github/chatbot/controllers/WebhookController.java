4beeb82e6ddab451337a4ba7b2d46b739b5d09d6
Vitor <vitornuneschagas2016@gmail.com>
2024-03-20 20:09:50 -0300

    rename methods name

--- src/main/java/com/github/chatbot/controllers/WebhookController.java
+++ src/main/java/com/github/chatbot/controllers/WebhookController.java
@@ -2,6 +2,7 @@ package com.github.chatbot.controllers;
 
 import com.github.chatbot.models.facebook.in.EventRequest;
 import com.github.chatbot.services.MessageSenderService;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,15 +18,16 @@ public class WebhookController {
     @Autowired
     public MessageSenderService messageSenderService;
     @GetMapping("/webhook")
-    public ResponseEntity<String> teste(@RequestParam("hub.mode") String hub, @RequestParam("hub.challenge") String challange, @RequestParam("hub.verify_token") String token ){
+    public ResponseEntity<String> verifyChallange(@RequestParam("hub.mode") String hub, @RequestParam("hub.challenge") String challange, @RequestParam("hub.verify_token") String token ){
         if(Objects.equals(token, "banana")){
             return new ResponseEntity<>(challange, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
+    @SneakyThrows
     @PostMapping("/webhook")
-    public ResponseEntity<String> teste2(@RequestBody EventRequest request) throws IOException, URISyntaxException, InterruptedException {
+    public ResponseEntity<String> botMessengerWebhook(@RequestBody EventRequest request){
         if(Objects.equals(request.getObject(), "page" )){
             messageSenderService.treatAndSendPostRequest(request);
             return new ResponseEntity<>(HttpStatus.OK);
