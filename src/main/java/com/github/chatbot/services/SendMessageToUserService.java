5a7d62b2a16ede18af489b5b6f72491875e0655c
Vitor <vitornuneschagas2016@gmail.com>
2024-03-31 21:28:18 -0300

    switch autowired to constructor

--- src/main/java/com/github/chatbot/services/SendMessageToUserService.java
+++ src/main/java/com/github/chatbot/services/SendMessageToUserService.java
@@ -2,7 +2,6 @@ package com.github.chatbot.services;
 
 import com.github.chatbot.models.facebook.in.EventRequest;
 import com.github.chatbot.models.facebook.out.MessageResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -13,19 +12,20 @@ import java.text.ParseException;
 
 @Service
 public class SendMessageToUserService {
-    @Autowired
-    private BotResponseBuilderService botResponseBuilderService;
     @Value("${facebook.apikey}")
     private String PAGE_TOKEN;
     @Value("${facebook.page.id}")
     private String PAGE_ID;
+    private final BotResponseBuilderService botResponseBuilderService;
+    public SendMessageToUserService(){
+        botResponseBuilderService = new BotResponseBuilderService();
+    }
     public void treatAndSendPostRequest(EventRequest request) throws IOException, ParseException {
         String psid = request.getPSID();
         String text = request.getUserMessage();
         MessageResponse response = botResponseBuilderService.build(psid, text);
         sendPostRequest(response);
     }
-
     private void sendPostRequest(MessageResponse responseBody){
         WebClient client =  WebClient.builder()
                 .baseUrl("https://graph.facebook.com/v19.0/" + PAGE_ID)
